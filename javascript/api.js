d3488663a1a60fd01b4dbd25c68df3b5cf2c6ec1
Folajimi Ogunbadejo <bekomay26@gmail.com>
2018-05-07 14:42:56 +0100

    update requests

--- javascript/api.js
+++ javascript/api.js
@@ -40,4 +40,55 @@ const getAbook = async(id) => {
         console.log(error)
     }
 }
-getAbook(4)
+
+const postBook = ( url, book) => {
+    try{
+        fetch(url, {
+            body: JSON.stringify(book),
+            method: 'POST',
+        })
+        .then(response => response.json());
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+const getPhotos = (url) => {
+    try{    
+    fetch(url)
+    .then(response => response.json());
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+
+function onAdd() {
+    const id = getBooks.length;
+    const title = document.getElementById("bookTitle").value;
+    const desc = document.getElementById("bookDesc").value;
+    const pageCount = document.getElementById("bookPage").value;
+    const excerpt = document.getElementById("bookExc").value;
+    const publishDate = document.getElementById("bookPubl").value;
+    
+    const book = {id, title, desc, pageCount, excerpt, publishDate};
+    postBook('http://fakerestapi.azurewebsites.net/api/Books', book);
+}
+
+function onPut(id){
+    const book = getAbook(id);
+    const title = document.getElementById("bookTitle").value || book.title;
+    const desc = document.getElementById("bookDesc").value || book.description;
+    const pageCount = document.getElementById("bookPage").value || book.pageCount;
+    const excerpt = document.getElementById("bookExc").value || book.excerpt;
+    const publishDate = document.getElementById("bookPubl").value || book.PublishDate;
+    
+    const newBbook = {id, title, desc, pageCount, excerpt, publishDate};
+    postBook(`http://fakerestapi.azurewebsites.net/api/Books/${id}`, newBook);
+}
+
+
+
+getBooks()
